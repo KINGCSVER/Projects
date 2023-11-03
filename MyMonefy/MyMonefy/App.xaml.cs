1c3b2284d0279739af81c08d98bf9ade38b0c6f4
Cavad Rahimli <cavad.rehimlicl@gmail.com>
2023-11-04 00:28:57 +0400

    First touch to design

---
+++ MyMonefy/MyMonefy/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MyMonefy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using SimpleInjector;
+using GalaSoft.MvvmLight;
+
+namespace MyMonefy
+{
+    public partial class App : Application
+    {
+        public static SimpleInjector.Container Container { get; set; } = new();
+
+        public void Register()
+        {
+
+            Container.RegisterSingleton<MainViewModel>();
+
+
+            Container.Verify();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Register();
+
+            var window = new MainView();
+
+            window.DataContext = Container.GetInstance<MainViewModel>();
+
+            window.ShowDialog();
+        }
+
+    }
+}
+
---
+++ MyMonefy/MyMonefy/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyMonefy.ViewModels
+{
+    internal class MainViewModel
+    {
+    }
+}
